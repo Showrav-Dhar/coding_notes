9e1d17326e788b101c1a59b61c437c57156e4ed8
Admin <admin@Admins-MacBook-Air.local>
2023-03-24 12:24:16 +0600

    initial commit

--- AlgorithmLearn.cpp
+++ AlgorithmLearn.cpp
@@ -240,7 +240,7 @@
 
 
                                 
-                                //          greedy method
+                                //          greedy method   //
 
 //              Fractional knapsack                //
 
