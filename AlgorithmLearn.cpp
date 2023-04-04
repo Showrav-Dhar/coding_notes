ed2c7824adfd7117fcf70407eb59d2ae8c28f309
showrav dhar <dhardip349900@gmail.com>
2023-04-04 19:29:52 +0600

    merge sort updated

--- AlgorithmLearn.cpp
+++ AlgorithmLearn.cpp
@@ -155,88 +155,89 @@
 
 // code ta bhul see strivers video 
 
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5;
+int ara[N];
 
-// const int N = 1e5;
+void MERGE(int low,int mid,int high)
+{
+    int Lid = low;
+    int Rid = mid+1;
+    int Tid = 0;
+    int temp[N];
+
+    while(Lid <= mid and Rid <= high)
+    {
+        if(ara[Lid] <= ara[Rid])
+        {
+            temp[Tid] = ara[Lid];
+            Lid++; Tid++;
+        }else{
+            temp[Tid] = ara[Rid];
+            Rid++; Tid++;
+        }
+    }
+
+    while (Lid<=mid)// imporatant point
+    {
+       temp[Tid] = ara[Lid];
+       Lid++;
+       Tid++;
+    }
+    while (Rid<=high)//important point 
+    {
+       temp[Tid] = ara[Rid];
+       Rid++;
+       Tid++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        ara[i] = temp[i-low];
+    }
+
+}
 
 
-// void MERGE(int *ara,int low,int mid,int high)
-// {
-//     int Lid = low;
-//     int Rid = mid+1;
-//     int Tid = 0;
-//     int temp[N];
+void MERGEsort(int low,int high)
+{
+    if(low == high){
+        return ;
+    }
 
-//     while(Lid <= mid and Rid <= high)
-//     {
-//         if(ara[Lid] <= ara[Rid])
-//         {
-//             temp[Tid] = ara[Lid];
-//             Lid++; Tid++;
-//         }else{
-//             temp[Tid] = ara[Rid];
-//             Rid++; Tid++;
-//         }
-//     }
+    int mid = (low+high)/2;
+    MERGEsort(low,mid);
+    MERGEsort(mid+1,high);
 
-//     while (Lid<=mid)
-//     {
-//        temp[Tid] = ara[Lid];
-//        Lid++;
-//        Tid++;
-//     }
-//     while (Rid<=mid)
-//     {
-//        temp[Tid] = ara[Rid];
-//        Rid++;
-//        Tid++;
-//     }
+    MERGE(low,mid,high);
 
-//     for (int i = low; i <= high; i++)
-//     {
-//         ara[i] = temp[i-low];
-//     }
+}
 
-// }
-// //
+int main()
+{
+    cout<<"Enter size\n";
+    int n;
+    cin>>n;
+    // int ara[n+10];
+    cout<<"Enter array items\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>ara[i];
+    }
 
-// void MERGEsort(int *ara,int low,int high)
-// {
-//     if(low>=high){ return ;}
+    int low = 0;
+    int high = n-1;
 
-//     int mid = (low+high)/2;
-//     MERGEsort(ara,low,mid);
-//     MERGEsort(ara,mid+1,high);
+    MERGEsort(low,high);
 
-//     MERGE(ara,low,mid,high);
-
-// }
-
-// int main()
-// {
-//     cout<<"Enter size\n";
-//     int n;
-//     cin>>n;
-//     int ara[n];
-//     cout<<"Enter array items\n";
-//     for (int i = 0; i < n; i++)
-//     {
-//         cin>>ara[i];
-//     }
-
-//     int low = 0;
-//     int high = n-1;
-
-//     MERGEsort(ara,low,high);
-
-//     for (int i = 0; i < n; i++)
-//     {
-//        cout<<ara[i]<<" ";
-//     }
+    for (int i = 0; i < n; i++)
+    {
+       cout<<ara[i]<<" ";
+    }
 
     
-// }
+}
 
 
                                 
@@ -244,70 +245,70 @@
 
 //              Fractional knapsack                //
 
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-bool cmp (pair<int,int> a, pair<int,int> b)
-{
-    double r1 = a.first/a.second;
-    double r2 = b.first/b.second;
+// bool cmp (pair<int,int> a, pair<int,int> b)
+// {
+//     double r1 = a.first/a.second;
+//     double r2 = b.first/b.second;
 
-    return r1>r2;
-}
+//     return r1>r2;
+// }
 
 
-double fractional_knapsack( vector< pair< int,int > > items,int capacity)
-{
-    int total_items = items.size();
-    double total_profits = 0;
+// double fractional_knapsack( vector< pair< int,int > > items,int capacity)
+// {
+//     int total_items = items.size();
+//     double total_profits = 0;
 
-    sort(items.begin(),items.end(),cmp);
+//     sort(items.begin(),items.end(),cmp);
 
-    for (int i = 0; i < total_items; i++)
-    {
-        if(capacity==0){
-            break;
-        }
+//     for (int i = 0; i < total_items; i++)
+//     {
+//         if(capacity==0){
+//             break;
+//         }
 
-        if(items[i].second<=capacity)
-        {
-            capacity -= items[i].second;//weight of item
-            total_profits += items[i].first;//profit of item
-        }else{
+//         if(items[i].second<=capacity)
+//         {
+//             capacity -= items[i].second;//weight of item
+//             total_profits += items[i].first;//profit of item
+//         }else{
 
-            double frac_profit = items[i].first * ( (double)capacity/ items[i].second );// item profit * (remaining capacity/item weight)
-            capacity-=capacity;
-            total_profits += frac_profit;
-        }
+//             double frac_profit = items[i].first * ( (double)capacity/ items[i].second );// item profit * (remaining capacity/item weight)
+//             capacity-=capacity;
+//             total_profits += frac_profit;
+//         }
         
-    }
+//     }
     
-    return total_profits;
-}
+//     return total_profits;
+// }
 
-int main()
-{
-    cout<<"Enter number of items\n";
-    int n;
-    cin>>n;
+// int main()
+// {
+//     cout<<"Enter number of items\n";
+//     int n;
+//     cin>>n;
 
-    vector< pair< int,int > > items(n);
-    cout<<"Enter profits and weights of the items\n";
-    int weights,profits;
-    for (int i = 0; i < n; i++)
-    {
-    //    cout<<"profits weights"<<endl;
-       cin>>profits>>weights;
-       items[i] = make_pair(profits,weights);
+//     vector< pair< int,int > > items(n);
+//     cout<<"Enter profits and weights of the items\n";
+//     int weights,profits;
+//     for (int i = 0; i < n; i++)
+//     {
+//     //    cout<<"profits weights"<<endl;
+//        cin>>profits>>weights;
+//        items[i] = make_pair(profits,weights);
        
-    }
+//     }
     
-    int capacity;
-    cout<<"Enter capacity of the knapsack\n";
-    cin>>capacity;
+//     int capacity;
+//     cout<<"Enter capacity of the knapsack\n";
+//     cin>>capacity;
 
-    double max_profit = fractional_knapsack(items,capacity);
-    cout<<"Maximum profit = "<<max_profit<<endl;
-   // okay git is working
+//     double max_profit = fractional_knapsack(items,capacity);
+//     cout<<"Maximum profit = "<<max_profit<<endl;
+//    // okay git is working
    
-}
+// }
