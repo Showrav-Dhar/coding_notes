15cfee4479822b811eeb3705606a4b6f131e8aef
showrav dhar <dhardip349900@gmail.com>
2023-04-28 01:40:27 +0600

    new file

---
+++ cps_academy.cpp
@@ -0,0 +1,53 @@
+
+
+/* class 7 */ 
+/* type casting */
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int a = 1e9;
+//     long long int c = (long long int)a*a;
+
+//     /* storing multiplication of a*a */
+
+//     cout<<c<<endl;
+// }
+
+/* modulus */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // cycle of mods;
+
+    int a = 17;
+    for (int i = 1; i <= 17; i++)
+    {
+        cout<<i%5<<" ";
+    }
+    
+    /*
+
+    1. kono decimal number ke 10 diye mod korle oi number er decimal 
+    representation er last digit pabo.
+
+    34 % 10 = 4;
+
+    2. kono decimal number ke 2 diye mod korle oi number er binary 
+    representation er last digit pabo.
+
+    34 = 100010
+
+    34%2 = 0; 0 is the last digit of the binary representation of 34
+
+    -> so kono decimal value ke jekono number system er base diye
+    mod korle oi number system er representation er last digit ta pawa jabe.
+    
+
+    */
+
+
+
+}
