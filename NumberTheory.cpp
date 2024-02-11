817227163e94499417fdcecb0468f8f33f0804cd
showrav dhar <dhardip349900@gmail.com>
2024-02-11 12:03:52 +0600

    Prime Factorization

--- NumberTheory.cpp
+++ NumberTheory.cpp
@@ -127,61 +127,143 @@
 // }
 
 //                  Sieve - prime generation
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long int ll ;
-const int mx = 1e7+123;
-ll ara[mx];
-bitset<mx>isPrime;
-vector<int>primes;
+// #include<bits/stdc++.h>
+// using namespace std;
+// typedef long long int ll ;
+// const int mx = 1e7+123;
+// ll ara[mx];
+// bitset<mx>isPrime;
+// vector<int>primes;
 
-void primeGen(ll n){
+// void primeGen(ll n){
     
-    // taking all the odds
-    // because only 2 is the even prime number
-    for (int i = 3; i <= n; i+=2)
+//     // taking all the odds
+//     // because only 2 is the even prime number
+//     for (int i = 3; i <= n; i+=2)
+//     {
+//         isPrime[i] = 1;
+//     }
+    
+//     for (int i = 3; i <= sqrt(n); i+=2)
+//     {
+//         if(isPrime[i]){
+//             for (int j = i*i ; j <= n; j+=i)
+//             {// only marking the multiples of previous prime numbers
+//                 isPrime[j] = 0;
+//             }
+            
+//         }
+//     }
+    
+//     primes.push_back(2);
+//     for (int i = 3; i <= n; i++)
+//     {
+//         if(isPrime[i]){
+//             primes.push_back(i);
+//         }
+//     }
+    
+    
+// }
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+    
+//     // see in the global space -
+//     // always use bitset<mx>isPrime; to store the isPrime array
+//     ll mx = 50;
+//     primeGen(mx);
+//     for(auto it : primes){
+//         cout<<it<<" ";
+//     }
+
+//     // c = a*b 
+//     // one of a and b will be less than sqrt(c) and one will be greater than sqrt(c)
+//     // 48 = 6*8 , sqrt(48) = 6.692 so 6 < 6.692
+//     // that is why in the outer loop i<=sqrt(n);
+
+    
+// }
+
+//              Prime Factorization 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+const int mx = 1e7 + 123;
+ll ara[mx];
+bitset<mx> isPrime;
+vector<int> primes;
+void primeGen(ll n)
+{
+    for (int i = 3; i <= n; i += 2)
     {
         isPrime[i] = 1;
     }
-    
-    for (int i = 3; i <= sqrt(n); i+=2)
+
+    for (int i = 3; i <= sqrt(n); i += 2)
     {
-        if(isPrime[i]){
-            for (int j = i*i ; j <= n; j+=i)
-            {// only marking the multiples of previous prime numbers
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
                 isPrime[j] = 0;
             }
-            
         }
     }
-    
+
     primes.push_back(2);
+    isPrime[2] = 1;
     for (int i = 3; i <= n; i++)
     {
-        if(isPrime[i]){
+        if (isPrime[i])
+        {
             primes.push_back(i);
         }
     }
-    
-    
 }
+
+vector<int> PrimeFactorization(int n)
+{
+    vector<int> factors;
+
+    for (auto it : primes)
+    {
+        if (n % it == 0)
+        {
+            if (1ll * it * it > n)
+            {
+                break;
+            // in factors of N there will be only 1 value which will be greater than sqrt(n)
+            }
+            while (n % it == 0)
+            {// if n%it then , dividing n with it until it is possible
+                factors.push_back(it);
+                n = n / it;
+            }
+        }
+    }
+
+    if (n > 1)// the last remaning n will always be prime
+    {// because-> in factors of N there will be only 1 value which will be greater than sqrt(n)
+        factors.push_back(n);
+    }
+    return factors;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    // see in the global space -
-    // always use bitset<mx>isPrime; to store the isPrime array
-    ll mx = 50;
-    primeGen(mx);
-    for(auto it : primes){
-        cout<<it<<" ";
+
+    int lim = 1e7;
+    primeGen(lim);
+    int n = 40131;
+    vector<int> ara = PrimeFactorization(n);
+    cout << "Prime Factors of " << n << " -> ";
+    for (auto it : ara)
+    {
+        cout << it << " ";
     }
-
-    // c = a*b 
-    // one of a and b will be less than sqrt(c) and one will be greater than sqrt(c)
-    // 48 = 6*8 , sqrt(48) = 6.692 so 6 < 6.692
-    // that is why in the outer loop i<=sqrt(n);
-
-    
+    cout << endl;
 }
