2f5d87778a897eea760b22e1f3267df1606db7fc
showrav dhar <dhardip349900@gmail.com>
2024-03-20 01:51:53 +0600

    DFS

--- Graph_Theory.cpp
+++ Graph_Theory.cpp
@@ -38,49 +38,102 @@
 // }
 
 // adjacent list representation
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long int ll;
-const int mx = 1e7 + 123;
-ll ara[mx];
-vector<pair<int, int>> adj[mx];
-//                              //  weighted Graph
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n, m;
-    cin >> n >> m; // node 4 ta , edge 3 ta
-    // graph -
-    // 4 3 // n,m,w
-    // 1 2 10
-    // 2 3 2
-    // 2 4 3
+// #include <bits/stdc++.h>
+// using namespace std;
+// typedef long long int ll;
+// const int mx = 1e7 + 123;
+// ll ara[mx];
+// vector<pair<int, int>> adj[mx];
+// //                              //  weighted Graph
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     int n, m;
+//     cin >> n >> m; // node 4 ta , edge 3 ta
+//     // graph -
+//     // 4 3 // n,m,w
+//     // 1 2 10
+//     // 2 3 2
+//     // 2 4 3
     
 
-    // multi graph example 
-    // 4 3 // n,m,w
-    // 1 2 10
-    // 1 2 13
-    // 1 2 14
-    // 2 3 2
-    // 2 4 3
-    for (int i = 1; i <= m; i++)
-    {
-        int u, v, w;
-        cin >> u >> v >> w;
-        adj[u].push_back({v, w});
-        adj[v].push_back({u, w});
-    }
+//     // multi graph example 
+//     // 4 3 // n,m,w
+//     // 1 2 10
+//     // 1 2 13
+//     // 1 2 14
+//     // 2 3 2
+//     // 2 4 3
+//     for (int i = 1; i <= m; i++)
+//     {
+//         int u, v, w;
+//         cin >> u >> v >> w;
+//         adj[u].push_back({v, w});
+//         adj[v].push_back({u, w});
+//     }
 
-    // use for each loop to access adjacency list
-    for (int i = 1; i <= n; i++)
-    {
-        cout << "adj list of node " << i << " : ";
-        for (auto it : adj[i])
-        {
-            cout << "Node " << it.first << " and cost : " << it.second << " , ";
-        }
-        cout << endl;
-    }
-}
+//     // use for each loop to access adjacency list
+//     for (int i = 1; i <= n; i++)
+//     {
+//         cout << "adj list of node " << i << " : ";
+//         for (auto it : adj[i])
+//         {
+//             cout << "Node " << it.first << " and cost : " << it.second << " , ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// DFS
+
+// Find Connected number of Components in a undirected graph 
+// Question - https://www.hackerearth.com/problem/algorithm/connected-components-in-a-graph/
+// #include<bits/stdc++.h>
+// using namespace std;
+// typedef long long int ll ;
+// const int mx = 1e7+123;
+// ll ara[mx];
+// vector<int>adj[mx];
+// bool vis[mx];
+
+// void dfs(int node){
+    
+//     vis[node] = true;
+//     for(auto child : adj[node]){
+//         if(vis[child]==false){
+//             dfs(child);
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+    
+//     int n,e;
+//     cin>>n>>e;
+//     for (int i = 0; i < e; i++)
+//     {
+//         int x,y;
+//         cin>>x>>y;
+//         adj[x].push_back(y);
+//         adj[y].push_back(x);
+//     }
+
+//     int ct = 0;
+//     for (int i = 1; i <= n; i++)// node count 1 theke hochhe
+//     {
+//         if(vis[i] == true){
+//             continue;
+//         }
+//         dfs(i);
+//         ct++;
+//     }
+//     cout<<ct<<endl;
+    
+    
+    
+// }
