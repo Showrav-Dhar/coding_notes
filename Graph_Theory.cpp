cba659b4523bd1b76fc740c261968b64682744fc
showrav dhar <dhardip349900@gmail.com>
2024-03-26 01:41:07 +0600

    cycle check

--- Graph_Theory.cpp
+++ Graph_Theory.cpp
@@ -139,30 +139,111 @@
 // }
 
 //    STORING ALL THE CONNECTED COMPONANTS
+// #include <bits/stdc++.h>
+// using namespace std;
+// typedef long long int ll;
+// const int mx = 1e7 + 123;
+
+// vector<vector<int>> cc; // all connected componants
+// vector<int> current_cc; // current connected componants
+// // to store the graph of currently running dfs
+
+// vector<int> adj[mx];
+// bool vis[mx];
+
+// void dfs(int node)
+// {
+//     // after entering the vertex we are adding it to the current_cc vector,
+//     vis[node] = true;
+//     current_cc.push_back(node);
+//     for (auto child : adj[node])
+//     {
+//         if (vis[child] == false)
+//         {
+//             dfs(child);
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, e;
+//     cin >> n >> e;
+//     for (int i = 0; i < e; i++)
+//     {
+//         int x, y;
+//         cin >> x >> y;
+//         adj[x].push_back(y);
+//         adj[y].push_back(x);
+//     }
+
+//     int ct = 0;
+//     // to store the connected componants
+
+//     for (int i = 1; i <= n; i++) // node count 1 theke hochhe
+//     {
+//         if (vis[i] == true)
+//         {
+//             continue;
+//         }
+//         current_cc.clear();// cleared the previously visited componant
+//         dfs(i);
+//         cc.push_back(current_cc);// stored the currently visited componant
+//         ct++;
+//     }
+
+//     cout<<"Number of connected componants = " << cc.size() << endl;
+//     int num = 1;
+//     for (auto componant : cc)
+//     {   
+//         cout<<"Connected Componant "<<num<<" : ";
+//         for (auto it : componant)
+//         {
+//             cout << it << " ";
+//         }
+//         num++;
+//         cout << endl;
+//     }
+
+//     // time complexity = O(v+e);
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
 const int mx = 1e7 + 123;
 
-vector<vector<int>> cc; // all connected componants
-vector<int> current_cc; // current connected componants
-// to store the graph of currently running dfs
+// Find cycle in a graph using dfs
 
 vector<int> adj[mx];
 bool vis[mx];
-
-void dfs(int node)
+            // 2           1
+            // 3           2
+            // 5           3
+bool dfs(int node, int parent)
 {
-    // after entering the vertex we are adding it to the current_cc vector,
     vis[node] = true;
-    current_cc.push_back(node);
+    bool isLoopExists = false;
+
     for (auto child : adj[node])
     {
-        if (vis[child] == false)
-        {
-            dfs(child);
+        if (vis[child] == true and child == parent)
+        { // backtrack er shomoi check kortesi
+            // current child er parent ke visited bhebe jeno cycle detect na kore.
+            continue;
         }
+        if (vis[child])// jehetu child ekbar visit korsi so cycle exist kore
+        {
+            return true;
+        }
+
+        isLoopExists = isLoopExists or dfs(child, node); // current child er parent = node
     }
+    return isLoopExists;
 }
 
 int main()
@@ -180,33 +261,35 @@ int main()
         adj[y].push_back(x);
     }
 
-    int ct = 0;
-    // to store the connected componants
+    // graph -
+    // 8 6
+    // 1 2
+    // 1 5
+    // 2 3
+    // 2 4
+    // 3 5
+    // 6 7
+    bool LoopExists = false;
 
-    for (int i = 1; i <= n; i++) // node count 1 theke hochhe
+    for (int i = 1; i <= n; i++)
     {
         if (vis[i] == true)
         {
             continue;
         }
-        current_cc.clear();// cleared the previously visited componant
-        dfs(i);
-        cc.push_back(current_cc);// stored the currently visited componant
-        ct++;
-    }
-
-    cout<<"Number of connected componants = " << cc.size() << endl;
-    int num = 1;
-    for (auto componant : cc)
-    {   
-        cout<<"Connected Componant "<<num<<" : ";
-        for (auto it : componant)
+        if (dfs(i, 0))// dfs(i,0) 0 disi cause 0 kono vertices nai question e.
         {
-            cout << it << " ";
+            LoopExists = true;
+            break;
         }
-        num++;
-        cout << endl;
     }
 
-    // time complexity = O(v+e);
+    if (LoopExists)
+    {
+        cout << "Cycle Exists in graph\n";
+    }
+    else
+    {
+        cout << "Cycle Does Not Exists in graph\n";
+    }
 }
