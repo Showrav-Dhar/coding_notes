a4219052c1945adc4ee4393b3ad0cc25aef64d2d
showrav dhar <dhardip349900@gmail.com>
2024-06-12 02:41:59 +0600

    fl

--- Graph_Theory.cpp
+++ Graph_Theory.cpp
@@ -1364,3 +1364,59 @@
 
 //     reverse(path.begin(),path.end());
 //     path.push_back(n);
+
+// Floyed warshall
+
+// class Solution
+// {
+// public:
+//     void shortest_distance(vector<vector<int>> &matrix)
+//     {
+//         int n = matrix.size();
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             for (int j = 0; j < n; j++)
+//             {
+//                 if (matrix[i][j] == -1)
+//                 {
+//                     matrix[i][j] = 1e9;
+//                 }
+//                 if (i == j)
+//                 {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+
+//         for (int k = 0; k < n; k++)
+//         {
+//             for (int i = 0; i < n; i++)
+//             {
+//                 for (int j = 0; j < n; j++)
+//                 {
+//                     matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j])
+//                 }
+//             }
+//         }
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             if(matrix[i][i] < 0){
+//                 cout<<"NEGATIVE CYCLE EXISTS"
+//             }
+//         }
+        
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             for (int j = 0; j < n; j++)
+//             {
+//                 if (matrix[i][j] == 1e9)
+//                 {
+//                     matrix[i][j] = -1;
+//                 }
+//             }
+//         }
+//     }
+// };
